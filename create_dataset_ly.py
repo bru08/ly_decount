1dc1ce31df866a904d7982cc1b7935197490b0e3
Bruno Papa <ppabrno@gmail.com>
2020-11-04 18:27:07 +0100

    revision (general)

---
+++ create_dataset_ly.py
@@ -0,0 +1,57 @@
+"""
+- Take the folders with extracted tiles 1 folder per slide with tiles inside
+- Take the folder with the .csv files from VIA annotations tools
+- match annotation files with patient id, then create annotations looking first to annotaiton file
+- Create symbolic link of tiles present in VIA annotations inside a train/valid folders
+- Create .txt files with annotations for the iamge with same stem in yolo format (normalized with image size)
+"""
+
+import os
+from pathlib import Path
+import re
+
+import pandas as pd
+
+IMG_SIDE = 512
+BB_SIDE_PX = 13
+l_class_id = [0]
+
+IMG_DS_DIR = Path("")
+RAW_ANNOTATIONS_DIR = Path("")
+DS_DIR = Path("./dataset")
+
+os.makedirs(DS_DIR / "train")
+os.makedirs(DS_DIR / "valid")
+
+
+# explore annotation folder
+ann_files = [x for x in os.listdir(RAW_ANNOTATIONS_DIR) if x.endswith(".csv") and re.search("_(\d+)_")]
+
+train_size = round(len(ann_files) * 0.75)
+tot_imgs, tot_annot = 0, 0
+
+# match annotated tiles and create symbolic links
+for i, annf in enumerate(ann_files):
+    dest_dir = "train" if i < train_size else "valid"
+    
+    slide_id = re.search("_(\d+)_", x).group(1)
+    df = pd.read_csv(RAW_ANNOTATIONS_DIR / annf)
+    
+    # create symlink for images
+    for tilename in df["filename"].unique():
+        os.symlink(IMG_DS_DIR / slide_id / tilename, DS_DIR / dest_dir / annf)
+        
+        # create annotations
+        tile_df = df.loc[df["filename"] == tilename, "region_shape_attributes"].values
+        centers = (json.loads(x) for x in tile_df)
+        norm_coords = [(elem.cx/IMG_SIDE, elem.cy/IMG_SIDE, BB_SIDE_PX/IMG_SIDE, BB_SIDE_PX/IMG_SIDE)]
+        
+        # write annotation file
+        with open(DS_DIR / dest_dir / re.sub(".png$", ".txt$", tilename), "w+") as f:
+            for coord in norm_coords:
+                f.write(",".join([str(x) for x in l_class_id+coord]))
+            
+            
+    
+    
+        
--- ly_regression/models.py
+++ ly_regression/models.py
@@ -98,33 +98,12 @@ class DenseMy(nn.Module):
         counts = self.count_head(flat)
         return loc_feats, counts
 
-# %%
-import numpy as np
-import matplotlib.pyplot as plt
-# %%
-def mat_gauss(ker=5, var=1):
-    mat = np.ones((ker,ker))
-    mi = ker // 2
 
-    for i in range(ker):
-        for j in range(ker):
-            mean_dist =  np.sqrt( (j - mi) ** 2 + (i - mi) ** 2 )
-            coef = 2 * np.pi * var
-            exp_coef = - 1 / (2 * var) 
-            p = coef * np.exp(exp_coef * (mean_dist ** 1.8))
-            mat[i,j] *= p
-    return mat
-# %%
-def count_dens_mask(coords, img_size=(199,199), var=12, spot_size=37):
-    img = np.zeros(img_size)
-    spot = mat_gauss(spot_size, var)
-    sshape = spot.shape
-    for pt in coords:
-        x_slice = slice(pt[0]-sshape[0]//2, pt[0]+sshape[0]//2 + 1)
-        y_slice = slice(pt[1]-sshape[0]//2, pt[1]+sshape[1]//2 + 1)
-        img[x_slice, y_slice] += spot
-    img /= np.max(img)
-    return img
-a = count_dens_mask([(50,100), (50,120)], var=30, spot_size=47)
-plt.imshow(a)
-# %%
+class DensUNet(nn.Module):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.base_model = torchvision.models.densenet121(pretrained=True)
+        self.base_layers = list(self.base_model.children())
