64b094a672070cab53a484aecfc7bdf2641e6dd8
bru08 <ppabrno@gmail.com>
2020-11-11 01:53:31 +0100

    train with density maps

--- C_count_dens_map/dens_count_example_b.py
+++ C_count_dens_map/dens_count_example_b.py
@@ -27,6 +27,8 @@ from PIL import Image
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from scipy.ndimage import gaussian_filter
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 # %%
 
 def load_lysto_weights(model, state_path):
@@ -51,76 +53,92 @@ def load_lysto_weights(model, state_path):
 
 # %%
 ENCODER_ARCH = "resnet50"
-PRETRAIN = "imagenet" # or lysto only for resnet50
+PRETRAIN = "lysto" # or lysto - imagenet only for resnet50
 lysto_checkpt_path = "/home/papa/ly_decount/A_lysto_regression/experiments/2020-11-08T16:43:50_resnetrefb_30ep_freeze_5ep_difflr/last.pth"
 FREEZE_ENCODER = True
 DATASET_DIR = "/home/riccardi/neuroblastoma_project_countCD3/try_yolo_ultralytics/dataset_nb_yolo_trail" 
 LR = 1e-3
-EPOCHS = 10
+EPOCHS = 5
+BATCH_SIZE = 16
 
 # %%
 class DMapData(torch.utils.data.Dataset):
 
-    def __init__(self, dataset_dir, mode, point_label_init=100, lbl_sigma_gauss=1):
+    def __init__(self, dataset_dir, mode, transformations, point_label_init=100, lbl_sigma_gauss=1):
         super().__init__()
         tmp_path = Path(dataset_dir) / mode
         self.pt_lbl_init = point_label_init
         self.lbl_sigma_gauss = lbl_sigma_gauss
         self.img_files = [tmp_path/x for x in os.listdir(tmp_path) if (tmp_path/x).suffix == ".png"]
+        self.transforms = transformations
 
     def __getitem__(self, idx):
-        img = np.array(Image.open(self.img_files[idx]).convert("RGB")) / 255.
+        img = np.array(Image.open(self.img_files[idx]).convert("RGB"))
         
         annot_file = re.sub(r".png$", r".txt", str(self.img_files[idx]))
         with open(annot_file, "r") as f:
             annots = f.read().split("\n")
         annots = [
                 [
-                    round(float(x) *  img.shape[0])
+                    int(float(x) *  (img.shape[0]-1))
                     for x in y.split(",")[1:3]
                     ] # take only xy of center in relative coord
             for y in annots if y
             ]
         mask_lbl = self.generate_label_mask(img, annots)
 
-        img = torchvision.transforms.ToTensor()(img)
-        mask_lbl = torchvision.transforms.ToTensor()(mask_lbl)
+        transformed = self.transforms(image=img, mask=mask_lbl)
 
-        return img, mask_lbl
+        img = transformed["image"] / 255.
+        mask = transformed["mask"]
 
-
+        return img, mask
 
     def generate_label_mask(self, img, annotations):
         mask = np.zeros(img.shape[:2])
         s = self.lbl_sigma_gauss
         for annot in annotations:
+
             mask[annot[1], annot[0]] = self.pt_lbl_init
         mask_blur = gaussian_filter(mask, (s,s), order=0)
         mask_blur /= mask_blur.sum()
         mask_blur *= (100 * len(annotations))
         return mask_blur
         
-    def show_example(self):
+    def show_example(self, with_mask=True):
         img, lbl = self.__getitem__(1)
         fig = plt.figure(figsize=(6,6))
-        plt.imshow(img)
-        plt.imshow(lbl, alpha=0.5)
+        plt.imshow(img.permute(1,2,0).numpy())
+        if with_mask:
+            plt.imshow(lbl.numpy(), alpha=0.5)
         plt.title(f"Sum: {lbl.sum():.2f}")
         plt.show()
 
     def __len__(self):
         return len(self.img_files)
 
-dataset_train = DMapData(DATASET_DIR, "train", lbl_sigma_gauss=6)
-# dataset_train.show_example()
-loader_train = DataLoader(dataset_train, batch_size=3, shuffle=True)
+
+
 
 # %%
-model = smp.Unet()
+# define albumentations transpose
+# execution is first to last
+transforms = A.Compose([
+    A.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.05, always_apply=False, p=0.99),
+    A.HorizontalFlip(p=0.5),
+    A.VerticalFlip(p=0.5),
+    ToTensorV2(),
+])
+dataset_train = DMapData(DATASET_DIR, "train", transforms, lbl_sigma_gauss=6)
+dataset_valid = DMapData(DATASET_DIR, "valid", transforms, lbl_sigma_gauss=6)
+# dataset_train.show_example(False)
+loader_train = DataLoader(dataset_train, batch_size=BATCH_SIZE, shuffle=True)
+loader_valid = DataLoader(dataset_valid, batch_size=BATCH_SIZE, shuffle=True)
+# %%
 if PRETRAIN == "imagenet":
-    model = smp.Unet(ENCODER_ARCH, encoder_weights=PRETRAIN)
+    model = smp.Unet(ENCODER_ARCH, encoder_weights=PRETRAIN, decoder_attention_type="scse")
 else:
-    model = smp.Unet(ENCODER_ARCH)
+    model = smp.Unet(ENCODER_ARCH, decoder_attention_type="scse")
 
 if PRETRAIN == "lysto":
     load_lysto_weights(model, lysto_checkpt_path)
@@ -143,24 +161,22 @@ segm_criterion = torch.nn.MSELoss()
 prob_cons_criterion = torch.nn.L1Loss()
 
 # %%
-
+losses_tr = dict(segment=[], conserv=[])
 for epoch in range(EPOCHS):
 
     model.train()
     for i, (img, mask) in enumerate(loader_train):
         # move data to device
         img = img.to(device).float()
-        mask = mask.to(device).float()
+        mask = mask.to(device).unsqueeze(1).float()
 
         # run inference
         out = model(img)
-        print(out.shape)
         # compute losses
         segm_loss = segm_criterion(out, mask)
-
         conservation_loss = prob_cons_criterion(out.sum(dim=[1,2,3]), mask.sum(dim=[1,2,3]))
         # losses aggregation
-        loss = segm_loss + conservation_loss
+        loss = segm_loss #+ conservation_loss
 
         # backward pass
         optimizer.zero_grad()
@@ -169,9 +185,54 @@ for epoch in range(EPOCHS):
 
         # log 
         print(
-            f"\rEpoch {epoch + 1}/{EPOCHS} loss:{loss.item():.2f}|segm_loss:{segm_loss.item():.2f} |cons_loss: {conservation_loss.item():.2f}",
+            f"\rEpoch {epoch + 1}/{EPOCHS} ({i+1}/{len(loader_train)}) loss:{loss.item():.4f}|segm_loss:{segm_loss.item():.2f} |cons_loss: {conservation_loss.item():.2f}",
             end="", flush=True
             )
+        # store losses
+        losses_tr["segment"].append(segm_loss.item())
+        losses_tr["conserv"].append(conservation_loss.item())
 
 
+# %%
+plt.plot(losses_tr["segment"])
+plt.yscale("log")
+
+# %%
+plt.plot(losses_tr["conserv"])
+plt.yscale("log")
+# %%
+img , msk = dataset_valid[48]
+# %%
+model.eval()
+with torch.no_grad():
+    out = model(img.unsqueeze(0).cuda())
+# %%
+plt.figure(figsize=(6,6))
+plt.imshow(img.cpu().permute(1,2,0).numpy())
+# %%
+plt.figure(figsize=(6,6))
+plt.imshow(msk.numpy())
+# %%
+plt.imshow(out.squeeze().cpu().numpy())
+# %%
+out.sum(), msk.sum()
+# %%
+fig, ax = plt.subplots(ncols=3, figsize=(18,6))
+img_d = img.cpu().permute(1,2,0).numpy()
+msk_out = out.squeeze().cpu().numpy()
+ax[0].imshow(img_d)
+ax[0].imshow(msk_out, alpha=0.85)
+ax[2].imshow(img_d)
+ax[1].imshow(msk)
+ax[1].set_title(f"target sum: {round(msk.sum().item()/100)}")
+ax[0].set_title(f"pred sum: {round(msk_out.sum().item()/100)}")
+plt.show()
+# %%
+msk_out.min(), msk_out.max()
+# %%
+tmp = msk_out - msk_out.min()
+# %%
+tmp.min()
+# %%
+plt.imshow(msk_out>(.5*msk_out.max()))
 # %%
