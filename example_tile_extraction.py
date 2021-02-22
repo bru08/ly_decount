ea3158d3a3b3bbb84cf5771c6f48c2bd5a15f1f4
bru08 <ppabrno@gmail.com>
2021-02-22 09:17:58 +0100

    refactoring example

--- example_tile_extraction.py
+++ example_tile_extraction.py
@@ -1,35 +1,36 @@
 """
-
+Create a tiler object and save an image contianing the tile extraction map
 """
 # %%
+from pathlib import Path
+from PIL import Image
+
 from histolab.tiler import GridTiler
 from histolab.slide import Slide
 
-from pathlib import Path
-import os
-import shutil
-from PIL import Image
-
-# %%
-TMP_EXT_DIR = Path(__file__).parent / 'temp_extraction' 
+#TODO CHANGE PATHS HERE!
 SVS_PATH = Path(__file__).parent / 'PDL_6.bif.tif'
+N_TILES = 175
+TILE_SIZE = 512
+EXTRACTION_LVL = 1
 
 # %%
-slide_obj= Slide(SVS_PATH, processed_path=".", slide_filters="texture")
+slide_obj= Slide(str(SVS_PATH), processed_path=".", slide_filters="texture")
+slide_obj
+# %%
 tiler = GridTiler(
-    tile_size= (512, 512),
-    level = 1,
+    tile_size= (TILE_SIZE, TILE_SIZE),
+    level = EXTRACTION_LVL,
     check_tissue=True,
     pixel_overlap= 0,
-    prefix=  str(TMP_EXT_DIR) + "/" + slide_obj.name + "_",
+    prefix=  str(Path(__file__).parent / slide_obj.name),
     suffix=".png",
-    partial=0.05,
-    maximum=170,
+    partial=0.99,
+    maximum=N_TILES,
 )
 
 # %%
+# create and save the extraction map as png
 plot = tiler.extraction_plot(slide_obj)
-# %%
 Image.fromarray(plot).save('tile_extraction_example.png')
-
 # %%
